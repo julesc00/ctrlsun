b471f55d2dde9f7f8695b35b9c0efc2952ce194f
juliobriones <julesc00@hotmail.com>
2021-04-15 16:45:52 -0500

    Added serializers..

---
+++ api/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from .models import NewUser, BranchLocation, WorkingTime
+
+
+class BranchLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BranchLocation
+        fields = ("id", "branch_name", "city", "country")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewUser
+        fields = ("id", "username", "password", "role", "branch")
+
+
+class WorkingTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkingTime
+        fields = ("id", "employee", "event_record", "action")
+
+
+
--- ctrlsun2/settings.py
+++ ctrlsun2/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'corsheaders',
+
     'api.apps.ApiConfig',
 ]
 
