8faabc5b1bd05d3158e1d1b5bf0ef1cb4ccb6dfb
juliobriones <julesc00@hotmail.com>
2021-04-15 21:58:05 -0500

    Added serialized data hyperlinking.

--- api/serializers.py
+++ api/serializers.py
@@ -6,27 +6,35 @@ from .models import NewUser, BranchLocation, WorkingTime
 
 
 class BranchLocationSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="branches-detail")
+
     class Meta:
         model = BranchLocation
-        fields = ("branch_name", "city", "country")
+        fields = ("branch_name", "city", "country", "url")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="users-detail")
+
     class Meta:
         model = NewUser
-        fields = ("username", "password", "role", "branch")
+        fields = ("username", "password", "role", "branch", "url")
 
 
 class StaffSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="staff-detail")
+
     class Meta:
         model = get_user_model()
-        fields = ("username", "password")
+        fields = ("username", "password", "url")
 
 
 class WorkingTimeSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="working-time-detail")
+
     class Meta:
         model = WorkingTime
-        fields = ("employee", "event_record", "action")
+        fields = ("employee", "event_record", "action", "url")
 
 
 
--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import SimpleRouter
 from .views import BranchLocationViewSet, UserViewSet, WorkingTimeViewSet, StaffViewSet
 
 router = SimpleRouter()
-router.register("branches", BranchLocationViewSet, basename="branches")
-router.register("users", UserViewSet, basename="users")
-router.register("staff", StaffViewSet, basename="staff")
-router.register("working-time", WorkingTimeViewSet, basename="working-time")
+router.register(r"branches", BranchLocationViewSet, basename="branches")
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"staff", StaffViewSet, basename="staff")
+router.register(r"working-time", WorkingTimeViewSet, basename="working-time")
 
 urlpatterns = router.urls
