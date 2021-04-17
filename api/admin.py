4c806e334d3011fc7dd71c8eaeba1cb53ff01723
juliobriones <julesc00@hotmail.com>
2021-04-16 21:21:39 -0500

    Added some docstrings.

--- api/admin.py
+++ api/admin.py
@@ -5,6 +5,8 @@ from .models import NewUser, BranchLocation, WorkingTime
 
 @admin.register(BranchLocation)
 class BranchAdmin(admin.ModelAdmin):
+    """Personalize the BranchLocation panel."""
+
     ordering = ("-branch_name",)
     search_fields = ("branch_name", "country", "city",)
     list_display = ("branch_name", "city", "country",)
@@ -15,6 +17,8 @@ class BranchAdmin(admin.ModelAdmin):
 
 @admin.register(NewUser)
 class EmployeeAdmin(admin.ModelAdmin):
+    """Personalize the custom user panel."""
+
     search_fields = ("username", "branch", "role",)
     ordering = ("-username",)
     list_display = ("username", "password", "role", "branch",)
@@ -25,6 +29,7 @@ class EmployeeAdmin(admin.ModelAdmin):
 
 @admin.register(WorkingTime)
 class WorkingTimeAdmin(admin.ModelAdmin):
+    """Personalize the employee working-time panel"""
     ordering = ("-event_record",)
     search_fields = ("employee", "event_record", "action",)
     list_display = ("employee", "event_record", "action",)
